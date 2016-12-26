a10e0e601a3e14267a429653626d654d67524ee6
woncheck <jwn7831@Naver.com>
2016-12-26 14:41:36 +0900

    added output "Bitkeeper"

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
               System.out.println("HELLO GIT!");
 		System.out.println("WHAT IS GIT?");
+		System.out.println("2002 Bitkeeper DVCS");
 	}
 }
 
