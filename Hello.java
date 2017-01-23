3b3f0c7e34abc70b94c4f40b1a5ad6272c94a83a
woncheck <jwn7831@Naver.com>
2017-01-23 14:37:34 +0900

    Hello.java modified on Local repository

--- Hello.java
+++ Hello.java
@@ -1,10 +1,14 @@
+// For commend line git tutorial
+// Local repository modification
+//
 public class Hello{
 	public static void main(String[] args){
               System.out.println("HELLO GIT!");
 	      System.out.println("What is GIt?");
 	      System.out.println("That's DVCS");
-		System.out.println("WHAT IS GIT?");
+		System.out.iprintln("WHAT IS GIT?");
 		System.out.println("2002 Bitkeeper DVCS");
+		
 	}
 }
 
